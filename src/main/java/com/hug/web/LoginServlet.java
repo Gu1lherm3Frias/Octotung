4f179e83a7fc565174c1a8da1c3ae0c90f5f453f
Guilherme Frias <guilherme.frias.o.t3@gmail.com>
2023-10-11 17:45:30 -0300

    [Feat]: Error handler added with servlet

--- src/main/java/com/hug/web/LoginServlet.java
+++ src/main/java/com/hug/web/LoginServlet.java
@@ -1,8 +1,9 @@
 package com.hug.web;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -15,19 +16,45 @@ public class LoginServlet extends HttpServlet{
         String email = req.getParameter("email");
         String password = req.getParameter("password");
         
-        if (email == null || password == null) {
-            req.setAttribute("Error", "Inputs missing");
-            doGet(req, res);
-        }else if (password != "123456") {
-            req.setAttribute("Error", "You password is incorrect");
-            doGet(req, res);
-        }else {
-            RequestDispatcher dispatcher = req.getRequestDispatcher("/index.jsp");
-            dispatcher.forward(req, res);
-        }
-    }
+        List<String> errors = new ArrayList<>();
 
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        getServletContext().getRequestDispatcher("/login.jsp").forward(req, res);
+        if (email == null || email.isBlank())
+            errors.add("E-mail is empty");
+        if (password == null || password.isEmpty())
+            errors.add("Password is empty");
+        if (email != null && !email.contains("@"))
+            errors.add("Invalid E-mail");
+        if (password != null && (password.length() < 8 || password.length() > 20))
+            errors.add("Password must have a minimum of 8 characters and max 20");
+        if (password != null) {
+            Boolean containLowerCase = false;
+            Boolean containUpperCase = false;
+            Boolean containDigits = false;
+
+            for (char c : password.toCharArray()) {
+                if (Character.isLowerCase(c))
+                    containLowerCase = true;
+                if (Character.isUpperCase(c))
+                    containUpperCase = true;
+                if (Character.isDigit(c))
+                    containDigits = true;
+            }
+
+            if (!containLowerCase)
+                errors.add("Password must have 1 Lowercase character");
+            if (!containUpperCase) 
+                errors.add("Password must have 1 Uppercase character");
+            if (!containDigits)
+                errors.add("Password must have digits");
+        }
+
+        if (errors.isEmpty()) {
+            res.sendRedirect("index.jsp");
+        }else {
+            req.setAttribute("email", email);
+            req.setAttribute("password", password);
+            req.setAttribute("errors", errors);
+            req.getRequestDispatcher("login.jsp").forward(req, res);
+        }
     }
 }
